9a2a2ce8416075c940a942f6183c7ff75bbe1013
Teerth Patel <teerth1205@gmail.com>
2020-01-23 21:05:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,7 +7,7 @@
   #end 
 #end 
 def prime?(value)
-  if value <= 1 || value == 0 || #value == 1
+  if value <= 1 || value == 0# || #value == 1
     return false
   elsif
     (2..value - 1).each do |i|
