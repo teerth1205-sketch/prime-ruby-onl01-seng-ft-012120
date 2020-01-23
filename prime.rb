a8cfeddcbf42f731d826e741f6c880a91a7d1c12
Teerth Patel <teerth1205@gmail.com>
2020-01-23 21:06:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+#def prime?(integer)
+#(2..integer -1).each do |int|
+ # if integer % int == 0
+  #   return false
+   #else 
+   #true 
+  #end 
+#end 
+def prime?(value)
+  if value <= 1 || value == 0# || #value == 1
+    return false
+  elsif
+    (2..value - 1).each do |i|
+      if value % i == 0
+        return false
+      end
+    end
+  end
+  true  # <= need explanation
+end
+
